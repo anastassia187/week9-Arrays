9feb0d1fba3acc72b144151cff122efb366e8be3
Anastassia Kuhartsik <nastjonka187@gmail.com>
2022-05-12 18:31:31 +0300

    arrays

---
+++ FortuneCookie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FortuneCookie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cookies = { "Win a Jackpot", "Fall in love", "Get a love letter", "Miss the bus", "See a unicorn un the street" };
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, cookies.Length);
+
+
+            Console.WriteLine($"Here is your prediction: {cookies[randomIndex]}!");
+        }
+    }
+}
---
+++ MatterOfTaste/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MatterOfTaste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color:");
+            string userColor = Console.ReadLine();
+
+            string[] colors = { "red", "green", "blue" };
+
+
+            for (int i = 1; i < colors.Length; i--)
+            {
+                if (userColor == "red")
+                {
+                    Console.WriteLine("We are perfect together!");
+
+                    break;
+
+                }
+                if (userColor == "green")
+                {
+                    Console.WriteLine("We are perfect together!");
+                    break;
+                }
+                if (userColor == "blue")
+                {
+                    Console.WriteLine("We are perfect together!");
+                    break;
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Every man to his taste.");
+                }
+
+                break;
+
+
+                    
+
+
+
+
+                }
+            }
+
+        }
+    }
+
+    
+
